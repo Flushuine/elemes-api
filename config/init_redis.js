8a82059217593afbaad0f929146b60eced38d8d5
Flushuine <aqillatas33@gmail.com>
2022-02-03 16:33:45 +0700

    Fix config

--- config/init_redis.js
+++ config/init_redis.js
@@ -1,6 +1,8 @@
 import redis from 'ioredis'
 
-const client = redis.createClient()
+const client = redis.createClient({
+    url: 'redis-11451.c1.asia-northeast1-1.gce.cloud.redislabs.com:11451'
+})
 
 client.on('error', function(error) {
     console.error('Error encountered: ', error)
